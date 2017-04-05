692afdccc9e8b64b01ed451e8ed050f5e4d7a000
Femi Falase <femi.falase@gmail.com>
2017-04-05 14:23:24 +0100

    Committed code 2017-04-07
    Signed-off-by: Femi Falase <femi.falase@gmail.com>

--- educationmanager/src/main/java/com/andychylde/commons/GeoPoliticalZone.java
+++ educationmanager/src/main/java/com/andychylde/commons/GeoPoliticalZone.java
@@ -2,6 +2,24 @@ package com.andychylde.commons;
 
 public enum GeoPoliticalZone {
 	
-	NORTH_CENTRAL,NORTH_EAST, NORTH_WEST, SOUTH_EAST, SOUTH_SOUTH, SOUTH_WEST;
+	NORTH_CENTRAL("Central Nigeria"),NORTH_EAST("North Eastern Nigeria"), NORTH_WEST("North Western Nigeria"),
+	SOUTH_EAST("South Eastern Nigeria"), SOUTH_SOUTH("The South South"), SOUTH_WEST("South Western Nigeria");
+	String gpzName;
 
+	GeoPoliticalZone(String gpzName) {
+		this.gpzName = gpzName;
+	}
+
+	public String getGpzName() {
+		return gpzName;
+	}
+
+	public void setGpzName(String gpzName) {
+		this.gpzName = gpzName;
+	}
+
+	@Override
+	public String toString() {
+		return this.name()+": " + this.getGpzName();
+	}
 }
