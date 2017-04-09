6032237ca1f7677d3dd1623cea0b5904ccf302fb
Femi Falase <femi.falase@gmail.com>
2017-04-09 03:57:24 +0100

    Committed code 2017-04-09
    Signed-off-by: Femi Falase <femi.falase@gmail.com>

--- educationmanager/src/main/java/com/andychylde/edusys/entities/Subject.java
+++ educationmanager/src/main/java/com/andychylde/edusys/entities/Subject.java
@@ -1,12 +1,21 @@
 package com.andychylde.edusys.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
 /**
  * Created by FEMI on 2017-04-05.
  * @author Femi Falase
  * @version 0.0.1
  */
 
-public class Subject {
+@Entity
+public class Subject implements Serializable{
+ @Id
+ private SubjectId subjectId;
  private SubjectType subjectType;
+ private String subjectName;
+
 
 }
---
+++ educationmanager/src/main/java/com/andychylde/edusys/entities/SubjectId.java
@@ -0,0 +1,7 @@
+package com.andychylde.edusys.entities;
+
+/**
+ * Created by FEMI on 2017-04-09.
+ */
+public class SubjectId {
+}
